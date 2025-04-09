4878b639105c535ec6e0e8c7bb16ce6128e642cf
Sean O'Reilly <seansoreilly@gmail.com>
2025-04-09 16:31:13 +1000

    add utility script to check costs

--- utils/aws_cost.ps1
+++ utils/aws_cost.ps1
@@ -2,6 +2,6 @@ $startDate = (Get-Date).AddHours(-24).ToString("yyyy-MM-dd")
 $endDate = (Get-Date).ToString("yyyy-MM-dd")
 
 aws ce get-cost-and-usage `
-  --time-period Start=$startDate,End=$endDate `
+  --time-period "Start=$startDate,End=$endDate" `
   --granularity DAILY `
   --metrics "AmortizedCost"
